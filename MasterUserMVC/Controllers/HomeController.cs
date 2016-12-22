8c52726485a09f3bf9b2406bce08b1d6731e6404
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-12-22 02:21:38 +0100

    refresh

--- MasterUserMVC/Controllers/HomeController.cs
+++ MasterUserMVC/Controllers/HomeController.cs
@@ -30,6 +30,8 @@ namespace MasterUserMVC.Controllers {
     //[Authorize(Roles = "Admin")]
     [System.Web.Mvc.Authorize]
     public ActionResult Admin() {
+      this.HttpContext.Response.AddHeader("refresh", "5; url=" + Url.Action("Admin"));
+
       List<string> loggedInUsers = (List<string>)HttpRuntime.Cache["LoggedInUsers"];
 
       return View();
